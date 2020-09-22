e844687b33cb5acbbf41216b404010752ab9d566
JXQI <2390139915@qq.com>
2020-09-22 14:35:11 +0800

    add the vgg

--- model.py
+++ model.py
@@ -57,9 +57,70 @@ class Alex_Net(nn.Module):
             x = torch.flatten(x, 1)
             x=self.classifier(x)
             return  x
+class VGG_Net(nn.Module):
+    def __init__(self):
+        super(VGG_Net,self).__init__()
+        self.features=nn.Sequential(
+            #block1
+            nn.Conv2d(3,64,3,padding=1,stride=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64,64,3,padding=1,stride=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+            #block2
+            nn.Conv2d(64,128,3,stride=1,padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128,128,3,stride=1,padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2,stride=2),
+            #block3
+            nn.Conv2d(128, 256, 3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, 3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            #block4
+            nn.Conv2d(256, 512, 3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # block5
+            nn.Conv2d(512, 512, 3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, 3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier=nn.Sequential(
+            #nn.Dropout(), #TODO:为何这里没有Dropout
+            nn.Linear(in_features=512*7*7,out_features=4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(in_features=4096,out_features=4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(in_features=4096,out_features=1000),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7)) #TODO:何时引入了全局平均池化
+    def forward(self,x):
+        x=self.features(x)
+        x = self.avgpool(x) #TODO:这一块什么时候引入的
+        x=x.flatten(x,1)
+        x=x.classifier(x)
+        return x
+
 
 if __name__=="__main__":
     # net=Net()
     # print(net.fc1)
-    alexnet=Alex_Net()
-    print(alexnet.features)
+    # alexnet=Alex_Net()
+    # print(alexnet.features)
+    vgg16=VGG_Net()
+    print(vgg16)
