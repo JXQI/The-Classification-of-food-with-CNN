c0cf518cc5db56deee5c9539a186ae49458254e4
JXQI <2390139915@qq.com>
2020-09-23 18:57:40 +0800

    add the googLeNet_v1

--- model.py
+++ model.py
@@ -116,11 +116,91 @@ class VGG_Net(nn.Module):
         x=x.classifier(x)
         return x
 
+def inception_1(x,input,output):
+    feature=nn.Sequential(
+        nn.Conv2d(in_channels=input,out_channels=output,kernel_size=1),
+        nn.ReLU(inplace=True),
+    )
+    return feature(x)
+def inception_3(x,input,_output,output):
+    feature=nn.Sequential(
+        nn.Conv2d(in_channels=input,out_channels=_output,kernel_size=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=_output,out_channels=output,kernel_size=3,padding=1),
+        nn.ReLU(inplace=True),
+    )
+    return feature(x)
+def inception_5(x,input,_output,output):
+    feature=nn.Sequential(
+        nn.Conv2d(in_channels=input,out_channels=_output,kernel_size=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(in_channels=_output,out_channels=output,kernel_size=5,padding=2),
+        nn.ReLU(inplace=True),
+    )
+    return feature(x)
+def inception_MaxPool(x,input,output):
+    feature=nn.Sequential(
+        nn.MaxPool2d(kernel_size=3,stride=1,padding=1),
+        nn.Conv2d(in_channels=input,out_channels=output,kernel_size=1),
+        nn.ReLU(inplace=True),
+    )
+    return feature(x)
+def inception(x,input,output1,_output3,output3,_output5,output5,_outputM):
+    x_1=inception_1(x,input,output1)
+    x_3=inception_3(x,input,_output3,output3)
+    x_5=inception_5(x,input,_output5,output5)
+    x_M=inception_MaxPool(x,input,_outputM)
+    outputs=[x_1,x_3,x_5,x_M]       #TODO:注意这里的聚合操作
+    outputs=torch.cat(outputs,1)
+
+    return outputs
+
+
+class GoogLeNet_v1(nn.Module):
+    def __init__(self,num_classes):
+        super(GoogLeNet_v1,self).__init__()
+        self.num_classes=num_classes
+        self.features1=nn.Sequential(
+            nn.Conv2d(3,64,7,stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3,stride=2),
+            nn.Conv2d(64,64,1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64,192,3,stride=1),
+            nn.MaxPool2d(3,stride=2),
+        )
+        self.maxpool=nn.MaxPool2d(3,stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
+        self.fc=nn.Linear(in_features=1024*1*1,out_features=self.num_classes)
+    def forword(self,x):
+        x=self.features1(x)
+        x=inception(x,192,64,96,128,16,32,192,32) #3a
+        x=inception(x,256,128,128,192,32,96,64) #3b
+        x=self.maxpool(x)
+        x=inception(x,480,192,96,208,16,48,64) #4a
+        x=inception(x,512,160,112,224,24,64,64) #4b
+        x=inception(x,512,128,128,256,4,64,64) #4c
+        x=inception(x,512,112,144,288,32,64,64) #4d
+        x=inception(x,528,256,160,320,32,128,128) #4e
+        x=self.maxpool(x)
+        x=inception(x,832,256,160,320,32,128,128) #5a
+        x=inception(x,832,384,92,384,48,128,28) #5b
+        #接入全局平均池化层
+        x=self.avgpool(x)
+        nn.Dropout()
+        x=self.fc(x)
+
+        return x
+
+
+
 
 if __name__=="__main__":
     # net=Net()
     # print(net.fc1)
     # alexnet=Alex_Net()
     # print(alexnet.features)
-    vgg16=VGG_Net()
-    print(vgg16)
+    # vgg16=VGG_Net()
+    # print(vgg16)
+    googLeNet=GoogLeNet_v1(1000)
+    print(googLeNet)
